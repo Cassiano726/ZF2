fec82ba1fda25d61869e2bcd047d78405aea4403
jerem <jerem@LAPTOP-O12409D4>
2016-10-20 16:22:05 -0200

    Projeto ZF2. Foi necessário rodar o composer. Livraria nao esta legal

--- config/application.config.php
+++ config/application.config.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * If you need an environment-specific system or application configuration,
- * there is an example in the documentation
- * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-system-configuration
- * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-application-configuration
- */
 return [
     // Retrieve list of modules used in this application.
-    'modules' => require __DIR__ . '/modules.config.php',
-
+    'modules' => require __DIR__ . '/modules.config.php',  '/livraria',
+    
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => [
         // This should be an array of paths in which modules reside.
